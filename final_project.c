aaa88e770b39b948bb5436630d98c766147dd011
Vasco Duarte <duarte-vasco@outlook.com>
2020-01-18 14:57:03 -0500

    Added the game part, still missing the seeds, the save/reload systems, the jokers and the levels.

--- final_project.c
+++ final_project.c
@@ -14,48 +14,196 @@
 #define MSG_ERROR "*** Illegal move"
 #define MSG_BYE "*** Sad to see you go..."
 #define MSG_UNKNOWN "*** Unknown option."
+#define MSG_WRONG "*** Woops... That's not correct."
+
+struct Player {
+    int level;
+    char difficulty[20];
+    int check;
+    char name[20];
+    char j50[10];
+    char j25[10];
+    int tries;
+} player;
+
+struct Answers {
+    char answer[20];
+    int right;
+    char letter;
+} answer;
+/* print the status of the player */
+
+void print_status()
+{
+    puts("********************************************");
+    printf("*** Name:  %s               *\n", player.name);
+    printf("*** level:  %d                  *\n", player.level);
+    printf("*** j50: %s                    *\n", player.j50);
+    printf("*** j25:  %s                   *\n", player.j25);
+    puts("********************************************");
+}
 
 /* The game itself */
 
-int game(int i)
+void game(int next_question)
 {
-    FILE *quest_file;
-    char answers [4];
-    int ascii = 65; 
-    int number = 200;
-    char buff[200];
-    char print_text [5000][200];
-    quest_file = fopen("questions.txt","r");
-    for (; i < 5; i++)
+    int infite = 1;
+    char choice;
+    while(infite)
     {
-        fgets(buff, number, (FILE *)quest_file);
-        strcpy(print_text[i], buff);
-        if (print_text[i][0] == 'O')
+        print_status();
+        FILE *quest_file;
+        int ascii = 65;
+        int i,z;
+        int set_free = 0;
+        char difficulty_check [20];
+        struct Answers answer_1;
+        struct Answers answer_2;
+        struct Answers answer_3;
+        struct Answers answer_4;
+        int game_condition = 0;
+        int test_1 = 0;
+        int test_2 = 0;
+        int test_3 = 0;
+        int test_4 = 0;
+        char buff[200];
+        char print_text [5000][200];
+        quest_file = fopen("questions.txt","r");
+        if (next_question == 0)
         {
-            strncpy(&answers[i-1],print_text[i] +8, 80);  
-            printf("*** %c: %s", ascii, &answers[i-1]);
-            ascii++;
+            for(z = 0; z < 300;z++)
+            {
+                fgets(buff, 200, (FILE *) quest_file);
+                strcpy(print_text[z],buff);
+            }
         }
-        else if (print_text[i][0] != 'O')
+        while(set_free == 0)
         {
-            printf("*** %s", print_text[i]);
+            strncpy(difficulty_check,print_text[6 + next_question] +11, 80);
+            printf("%c||%c", difficulty_check[0], player.difficulty[0]);
+            if (difficulty_check[0] == player.difficulty[0])
+            {
+                for (i = 0;i < 5 ;i++)
+                {
+                    if (i == 0)
+                    {
+                        printf("%s", print_text[i + next_question]);
+                    }
+                    if (i == 1)
+                    {
+                        strncpy(answer_1.answer,print_text[i + next_question] +8, 80);
+                    }
+                    if (i == 2)
+                    {
+                        strncpy(answer_2.answer,print_text[i + next_question] +8, 80);
+                    }
+                    if (i == 3)
+                    {
+                        strncpy(answer_3.answer,print_text[i + next_question] +8, 80);
+                    }
+                    if (i == 4)
+                    {
+                        strncpy(answer_4.answer,print_text[i + next_question] +8, 80);
+                    }
+                    set_free = 1;
+                }
+            }
+            else if (difficulty_check[0] != player.difficulty[0])
+            {
+                next_question += 7;
+            }
+        }
+        i = 0;
+        while (i <= 3)
+        {
+            int e = rand() %4;
+            if (e == 0 && test_1 < 1)
+            {
+                printf("*** %c: %s", ascii, answer_1.answer);
+                answer_1.letter = ascii;
+                ascii++;
+                test_1 = 1;
+                i++;
+            }
+            if (e == 1 && test_2 < 1)
+            {
+                printf("*** %c: %s", ascii, answer_2.answer);
+                answer_2.letter = ascii;
+                ascii++;
+                test_2 = 1;
+                i++;
+            }
+            if (e == 2 && test_3 < 1)
+            {
+                printf("*** %c: %s", ascii, answer_3.answer);
+                answer_3.letter = ascii;
+                ascii++;
+                test_3 = 1;
+                i++;
+            }
+            if (e == 3 && test_4 < 1)
+            {
+                printf("*** %c: %s", ascii, answer_4.answer);
+                answer_4.letter = ascii;
+                ascii++;
+                test_4 = 1;
+                i++;
+            }
+        }
+        while (game_condition != 1)
+        {
+            char command[20];
+            char option;
+
+            printf(">");
+            fgets(command, 100, stdin);
+            sscanf(command, "%c %s", &choice, &option);
+
+            if (choice == answer_1.letter)
+            {
+                puts(MSG_WELL);
+                next_question += 7;
+                game_condition = 1;
+                player.level += 1000;
+            }
+            else if (choice == answer_2.letter)
+            {
+                puts(MSG_WRONG);
+                printf("*** The correct answer was %c: %s", answer_1.letter,answer_1.answer);
+                player.tries++;
+                next_question += 7;
+                game_condition = 1;
+            }
+            else if (choice == answer_3.letter)
+            {
+                puts(MSG_WRONG);
+                printf("*** The correct answer was %c: %s", answer_1.letter,answer_1.answer);
+                fclose(quest_file);
+                player.tries++;
+                next_question += 7;
+                game_condition = 1;
+            }
+            else if (choice == answer_4.letter)
+            {
+                puts(MSG_WRONG);
+                printf("*** The correct answer was %c: %s", answer_1.letter,answer_1.answer);
+                fclose(quest_file);
+                player.tries++;
+                next_question += 7;
+                game_condition = 1;
+            }
+            else if (choice == 'q')
+            {
+                return;
+            }
+            else
+            {
+                puts(MSG_UNKNOWN);
+            }
+            
         }
     }
-    fclose(quest_file);
-    return i + 8;
 
-}
-
-/* print the status of the player */
-
-void print_status(const int level, const char * j50, const char * j25, const char * name)
-{
-    puts("********************************************");
-    printf("*** Name:  %s               *\n", name);
-    printf("*** level:  %02d                  *\n", level);
-    printf("*** j50: %s                    *\n", j50);
-    printf("*** j25:  %s                   *\n", j25);
-    puts("********************************************");
 }
  
 /* print the option menu */
@@ -96,20 +244,24 @@ int main(int argc, char * argv[])
     /*Those are the valors that we will use*/
 
     char command[20];
-    char choice;
     char option;
-    int level = 0;
-    int tries = 0;
+    char choice;
+
+    player.level = 0;
+    player.tries = 0;
+    player.check = 0;
+    strcpy(player.name, "Newbie");
+    strcpy(player.difficulty, "easy");
+    strcpy(player.j50, "Yes");
+    strcpy(player.j25, "Yes");
+    int next_question = 0;
+
     int check = 0;
-    int i = 0;
-    char text[] =".txt";
-    char j50[] = "Yes";
-    char j25[] = "Yes";
-    char name[20] = "Newbie";
+    char text[] = ".txt";
 
     /*This while will cause the game to continue until you quit/lose/win*/
 
-    while(command[0] != 'q' && check != 100000 && tries != 2)
+    while(command[0] != 'q' && check != 100000 && player.tries != 2)
     {
         printf(">");
         fgets(command, 100, stdin);
@@ -119,17 +271,17 @@ int main(int argc, char * argv[])
         {
             if (option)
             {
-                char new_name[20];
-                strcpy(new_name ,&option);
-                printf("*** Hi %s, let's get started!\n", new_name);
-                strcpy(name, new_name);
+                strcpy(player.name,&option);
+                printf("*** Hi %s, let's get started!\n", player.name);
+                check = 1;
             }
             if (check == 0)
             {
-                puts("*** Hi Newbie, let's get started!");
-                print_status(level, j50, j25, name);
+                strcpy(player.name,"Newbie");
+                printf("*** Hi %s, let's get started!\n", player.name);
             }
-            game(i);
+            game(next_question);
+            return (0);
         }
         else if (choice == 'r')
         {
@@ -139,9 +291,9 @@ int main(int argc, char * argv[])
             strcat(file_name, text);
             FILE *save_file; 
             save_file = fopen(file_name,"r");
-            fprintf(save_file, "%d\n" , level);
+            fprintf(save_file, "%d\n" , player.level);
             char name[] = "boomer";
-            printf("Ok %s, where were we? Oh there you go:\n", name);
+            printf("*** Ok %s, where were we? Oh there you go:\n", name);
             /* game(points, level, j50, j25); */
         }
 
@@ -155,10 +307,10 @@ int main(int argc, char * argv[])
                 strcat(file_name, text);
                 FILE *save_file; 
                 save_file = fopen(file_name,"w");
-                fprintf(save_file, "%d\n" , level);
-                fprintf(save_file, "%s\n" , j50);
-                fprintf(save_file, "%s\n" , j25);
-                fprintf(save_file, "%s\n" , name);
+                fprintf(save_file, "%d\n" , player.level);
+                fprintf(save_file, "%s\n" , player.j50);
+                fprintf(save_file, "%s\n" , player.j25);
+                fprintf(save_file, "%s\n" , player.name);
                 fclose(save_file);
                 puts(MSG_SAVE);
             }
@@ -177,10 +329,10 @@ int main(int argc, char * argv[])
 
         else if (choice == 'c')
         {
-            puts("This game was made by :");
-            puts("Vasco Duarte (21905658)");
-            puts("Tony (gne gne)");
-            puts("Joao Magic (gagagou)");
+            puts("*** This game was made by :");
+            puts("*** Vasco Duarte (21905658)");
+            puts("*** Tony (gne gne)");
+            puts("*** Joao Magic (gagagou)");
         }
         
         else
